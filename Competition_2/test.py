416a7396cc30a0d72f6d7c815876b12af810a76f
eakr9014 <54963080+eakr9014@users.noreply.github.com>
2020-03-04 13:47:05 -0700

    code for output

--- Competition_2/test.py
+++ Competition_2/test.py
@@ -15,12 +15,15 @@ from keras.optimizers import SGD
 
 #create DF and split into train and test
 stockDF = pd.read_csv("stock_XY_train.csv")
-stockDF = stockDF.dropna(axis='columns', thresh=stockDF.shape[0]*.9)
-stockDF = stockDF.dropna()
+stockTest = pd.read_csv("stock_X_test.csv")
+stockDF = stockDF.dropna(axis='columns', thresh=stockDF.shape[0]*.75)
+# stockDF = stockDF.dropna()
 stockDF = stockDF.drop(stockDF.columns[[0,1]], axis=1)
+stockTest_names = stockTest["labels"]
+stockTest = stockTest.drop(stockTest.columns[[0,1,2]], axis=1)
 stockDF_y = stockDF["Buy"]
 stockDF = stockDF.drop(columns=["Yr", "Buy", "Sector"])
-
+stockTest = stockTest.drop(columns=["Yr", "Sector"])
 
 
 """TRYING OUT A TREE MODEL AND FINDING MOST IMPORTANT FEATURES"""
@@ -28,6 +31,7 @@ stockDF = stockDF.drop(columns=["Yr", "Buy", "Sector"])
 regr = AdaBoostRegressor(random_state=0, learning_rate = .5,
 base_estimator=DecisionTreeRegressor(max_depth=16, max_features="sqrt"),
 n_estimators=13, loss = "linear")
+stockDF = stockDF.fillna(stockDF.mean())
 regr.fit(stockDF,stockDF_y);
 
 feature_names= stockDF.columns
@@ -38,7 +42,10 @@ most_important_features=[name for val,name in columns_tuple]
 # print(len(most_important_features))
 
 stockDF=stockDF[most_important_features[0:35]]
+stockTest=stockTest[most_important_features[0:35]]
 stockDF=(stockDF-stockDF.mean())/stockDF.std() #normailize data
+stockTest=(stockTest-stockTest.mean())/stockTest.std() #normailize data
+stockTest = stockTest.fillna(stockTest.mean())
 train_X, val_X, train_y, val_y = train_test_split(stockDF, stockDF_y, train_size=.8, shuffle=True)
 
 #testing!
@@ -49,17 +56,32 @@ print(stockDF.dtypes)
 model = keras.Sequential()
 
 #add layers to model
-model.add(Dense(50, input_dim= stockDF.shape[1], activation="relu"))
-model.add(Dense(30, activation="relu"))
-model.add(Dropout(.2))
-model.add(Dense(10, activation="relu"))
-model.add(Dense(40, activation="relu"))
-model.add(Dropout(.05))
-model.add(Dense(5, activation="relu"))
-
-
+model.add(Dense(35, input_dim= stockDF.shape[1], activation="relu"))
+model.add(Dropout(.15))
+model.add(Dense(16, activation="relu"))
+model.add(Dropout(.15))
+model.add(Dense(32, activation="relu"))
+model.add(Dropout(.15))
 model.add(Dense(1, activation="sigmoid"))
 
 #compile model
 model.compile(optimizer='adam', loss='mean_squared_logarithmic_error', metrics=['accuracy'])
-model.fit(train_X,train_y, validation_data=(val_X, val_y), epochs=80)
+model.fit(train_X,train_y, validation_data=(val_X, val_y), epochs=20)
+
+#test and output model
+pred = model.predict(stockTest)
+print(stockTest_names.shape)
+pred_labels = []
+print(pred)
+for arr in pred:
+    if np.isnan(arr[0]) ==  True:
+        pred_labels.append(1)
+    elif arr[0] < .5:
+        pred_labels.append(0)
+    else:
+        pred_labels.append(1)
+print(pred_labels)
+pred_labels = np.asarray(pred_labels)
+output_dict = {"Unnamed: 0" : stockTest_names, "Buy" : pred_labels}
+output = pd.DataFrame(data = output_dict)
+output.to_csv("submission.csv", index=False)
